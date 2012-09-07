5935e7ccf2ab17bc3a6ebfa9bd80427373b2adcc
lissy.lau <lissy.lau@68a7bdbe-0cd2-11de-b55b-d397e7115e68>
2012-09-07 16:18:54 +0000

    Module Makefile update is finished!

--- trunk/App/Analyzer/FrequencyAnalyzer/bld/LinuxConsoleApp.mk
+++ trunk/App/Analyzer/FrequencyAnalyzer/bld/LinuxConsoleApp.mk
@@ -23,21 +23,24 @@ DEF_MOD :=
 #==============================================================================
 
 OPT_ASM_MOD :=
+
 OPT_CC_MOD  :=
+
 OPT_CXX_MOD :=
-OPT_LD_MOD  :=
+
+OPT_LD_MOD  := \
+            -lCharFrequency
 
 
 #==============================================================================
 # Module Include Paths
 #================================================= =============================
 
-INCLUDE_MOD := \
-            -I$(MOD_DIR)/include
+INCLUDE_MOD :=
 
 
 #==============================================================================
-# List of objects to link
+# List of Objects to Link
 #==============================================================================
 
 OBJS_TO_LINK := \
@@ -48,74 +51,14 @@ OBJS_TO_LINK := \
 # External Libraries
 #==============================================================================
 
-LIBS_TO_LINK :=
-
-OBJECTS :=
-
-LIBRARIES := \
-    $(LIB_DIR)/libCharFrequency.so
-
-LD_LIBS := \
-    -lCharFrequency
-
-INCLUDES := \
-    -I$(INC_DIR)
-
-LDPATHS := \
-    -L$(LIB_DIR)
-
-CXXFLAGS := -g -fno-builtin -Wall -Wwrite-strings -Wsign-compare -Werror $(DEF_MOD) $(INCLUDES)
-CFLAGS   := -g -fno-builtin -Wall -Wwrite-strings -Wsign-compare -Werror $(DEF_MOD) $(INCLUDES)
-
-# Rules: Must use TAB before the command.
-
-.DEFAULT_GOAL := $(BUILD_TARGET)
-
-all: $(BUILD_TARGET)
-
-$(BUILD_TARGET): $(OBJ_DIR)/FrequencyAnalyzer_Main.o $(OBJECTS) $(LIBRARIES)
-	$(CXX) -o $@ $(OBJ_DIR)/FrequencyAnalyzer_Main.o $(OBJECTS) $(LDPATHS) $(LD_LIBS)
-
-$(OBJ_DIR)/FrequencyAnalyzer_Main.o: $(MOD_DIR)/FrequencyAnalyzer_Main.cpp $(OBJECTS) $(LIBRARIES)
-	$(CXX) -c $(CXXFLAGS) -o $(OBJ_DIR)/FrequencyAnalyzer_Main.o $(MOD_DIR)/FrequencyAnalyzer_Main.cpp
-
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cc $(INC_DIR)/%.hpp
-	$(CXX) -c $(CXXFLAGS) -o $@ $<
-
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp $(INC_DIR)/%.hpp
-	$(CXX) -c $(CXXFLAGS) -o $@ $<
-
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cxx $(INC_DIR)/%.hpp
-	$(CXX) -c $(CXXFLAGS) -o $@ $<
-
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c $(INC_DIR)/%.h
-	$(CC) -c $(CFLAGS) -o $@ $<
-
-$(LIB_DIR)/lib%.so: $(OBJ_DIR)/%.o
-	$(CC) $(CFLAGS) -shared -o $@ $< -lstdc++
-
-# Use - before the command to ignore error.
-
-install:
-	-mkdir -p $(INSTALL_BIN_PATH)
-	-mkdir -p $(INSTALL_LIB_PATH)
-	-cp $(BUILD_TARGET) $(INSTALL_BIN_PATH)
-	-cp $(LIBRARIES) $(INSTALL_LIB_PATH)
-
-clean:
-	-rm -f $(OBJ_DIR)/*
-
-cleanlib:
-	-rm -f $(LIB_DIR)/*
-
-cleanall: clean cleanlib
-	-rm -f $(BIN_DIR)/*
+LIBS_TO_LINK := \
+             $(LIB_DIR)/libCharFrequency.so
 
 
 #==============================================================================
 # Include the standard build rules
 #==============================================================================
 
-#include $(RULES_ROOT)/rules.default
+include $(RULES_ROOT)/rules.default
 
 
